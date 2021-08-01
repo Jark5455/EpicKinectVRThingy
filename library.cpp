1f0dba6c8c40c5edf5761352142dc96235f5fc71
yash <rshailu.yash@gmail.com>
2021-07-31 21:01:00 -0400

    Removed UpdateUserState function

--- library.cpp
+++ library.cpp
@@ -5,56 +5,13 @@
 
 using namespace nite;
 
-#define MAX_USERS 1
-bool g_visibleUsers[MAX_USERS] = {false};
-nite::SkeletonState g_skeletonStates[MAX_USERS] = {nite::SKELETON_NONE};
-
-#define USER_MESSAGE(msg) \
-{printf("[%08llu] User #%d:\t%s\n",ts, user.getId(),msg);}
-
-const nite::UserData *userdata = new UserData();
+const nite::UserData *userdata;
 
 long joystick_x = 130;
 long joystick_y = 130;
 bool joystick_c = false;
 bool joystick_z = false;
 
-void updateUserState(const nite::UserData& user, unsigned long long ts)
-{
-    if (user.isNew())
-        USER_MESSAGE("New")
-    else if (user.isVisible() && !g_visibleUsers[user.getId()])
-        USER_MESSAGE("Visible")
-    else if (!user.isVisible() && g_visibleUsers[user.getId()])
-        USER_MESSAGE("Out of Scene")
-    else if (user.isLost())
-        USER_MESSAGE("Lost")
-
-    g_visibleUsers[user.getId()] = user.isVisible();
-
-    if(g_skeletonStates[user.getId()] != user.getSkeleton().getState())
-    {
-        switch(g_skeletonStates[user.getId()] = user.getSkeleton().getState())
-        {
-        case nite::SKELETON_NONE:
-            USER_MESSAGE("Stopped tracking.")
-            break;
-        case nite::SKELETON_CALIBRATING:
-            USER_MESSAGE("Calibrating...")
-            break;
-        case nite::SKELETON_TRACKED:
-            USER_MESSAGE("Tracking!")
-            break;
-        case nite::SKELETON_CALIBRATION_ERROR_NOT_IN_POSE:
-        case nite::SKELETON_CALIBRATION_ERROR_HANDS:
-        case nite::SKELETON_CALIBRATION_ERROR_LEGS:
-        case nite::SKELETON_CALIBRATION_ERROR_HEAD:
-        case nite::SKELETON_CALIBRATION_ERROR_TORSO:
-            USER_MESSAGE("Calibration Failed... :-|")
-            break;
-        }
-    }
-}
 
 [[noreturn]] void startUserTracking() {
     UserTracker userTracker;
